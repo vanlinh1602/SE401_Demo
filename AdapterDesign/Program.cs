df3a16f4b9a1c1aafb2dea7e0cc4832d3398d134
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2024-03-18 23:15:31 +0700

    add adapter design

---
+++ AdapterDesign/Program.cs
@@ -0,0 +1,7 @@
+﻿List<IShape> shapes = new List<IShape>() { new LineAdapter(new LegacyLine()), new RectangleAdapter(new LegacyRectangle()) };
+
+int x1 = 5, y1 = 10, x2 = -3, y2 = 2;
+
+shapes.ForEach(shape => shape.Draw(x1, y1, x2, y2));
+
+
